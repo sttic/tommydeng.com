f47ba4a5cf02c3bb2f203e41cf0d9b3a9ea8fd7b
sttic <tommyd3689@gmail.com>
2019-02-16 23:44:54 -0500

    refactored contact page

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -3,76 +3,88 @@
     <header>
       <h2>Contact Information</h2>
       <p>Feel free to share your thoughts and connect with me.</p>
-
       <ul class="icons">
-        <li>
-          <a href="https://www.linkedin.com/in/tommydeng" class="icon fa-linkedin">
-            <span class="label">LinkedIn</span>
-          </a>
-        </li>
-        <li>
-          <a href="mailto:contact@tommydeng.com" target="_blank" class="icon fa-envelope">
-            <span class="label">Email</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/sttic" class="icon fa-github">
-            <span class="label">Github</span>
-          </a>
+        <li v-for="(item, index, id) in contactIcons" :key="id">
+          <a :href="item.link" :class="item.icon"></a>
         </li>
       </ul>
     </header>
 
     <div class="box">
-      <h3>General</h3>
-      <ul class="alt">
-        <li>Name
-          <span style="float:right;">Tommy Deng</span>
-        </li>
-        <li>Location
-          <span style="float:right;">Ottawa, Ontario</span>
-        </li>
-        <li>Email
-          <span style="float:right;">contact@tommydeng.com</span>
-        </li>
-        <li>LinkedIn
-          <span style="float:right;">linkedin.com/in/tommydeng</span>
-        </li>
-        <li>Github
-          <span style="float:right;">github.com/sttic</span>
-        </li>
-      </ul>
-
-      <h3>Education</h3>
-      <ul class="alt">
-        <li>Program
-          <span style="float:right;">BASc Software Engineering</span>
-        </li>
-        <li>Year of study
-          <span style="float:right;">2</span>
-        </li>
-        <li>Expected
-          <span style="float:right;">Dec 2021</span>
-        </li>
-      </ul>
-
-      <h3>Employment</h3>
-      <ul class="alt">
-        <li>Status
-          <span style="float:right;">Seeking position</span>
-        </li>
-        <li>Availability
-          <span style="float:right;">Fall 2019</span>
-        </li>
-      </ul>
+      <div v-for="(item, index, id) in contactInfo" :key="id">
+        <h3>{{ item.title }}</h3>
+        <ul class="contact alt">
+          <li v-for="(item, index, id) in item.infoList" :key="id">
+            {{ item.type }}
+            <a v-if="item.link" :href="item.link">{{ item.data }}</a>
+            <span v-else>{{ item.data }}</span>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
 
+<script>
+export default {
+  name: "ContactInfo",
+  data() {
+    return {
+      contactIcons: [
+        {
+          icon: "icon fa-linkedin",
+          link: "https://www.linkedin.com/in/tommydeng"
+        },
+        { icon: "icon fa-envelope", link: "mailto:contact@tommydeng.com" },
+        { icon: "icon fa-github", link: "https://github.com/sttic" }
+      ],
+      contactInfo: [
+        {
+          title: "General",
+          infoList: [
+            { type: "Name", data: "Tommy Deng" },
+            { type: "Location", data: "Ottawa, ON" },
+            {
+              type: "LinkedIn",
+              data: "linkedin.com/in/tommydeng",
+              link: "https://www.linkedin.com/in/tommydeng"
+            },
+            {
+              type: "Email",
+              data: "contact@tommydeng.com",
+              link: "mailto:contact@tommydeng.com"
+            },
+            {
+              type: "Github",
+              data: "github.com/sttic",
+              link: "https://github.com/sttic"
+            }
+          ]
+        },
+        {
+          title: "Education",
+          infoList: [
+            { type: "Program", data: "BASc Software Engineering" },
+            { type: "Year of study", data: "2" },
+            { type: "Expected", data: "Dec 2021" }
+          ]
+        },
+        {
+          title: "Employment",
+          infoList: [
+            { type: "Status", data: "Seeking position" },
+            { type: "Availability", data: "Fall 2019" }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
+
 <style scoped>
-#main {
-  /* margin-top: none; */
-  padding: 4em 0;
+ul.contact span,
+ul.contact a {
+  float: right;
 }
 </style>
-
