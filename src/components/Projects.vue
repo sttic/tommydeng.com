414c676a7195e1d5966b85d936d368a512f0c5da
sttic <tommyd3689@gmail.com>
2019-02-18 21:42:09 -0500

    refactored projects with list rendering

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,309 +1,166 @@
 <template>
   <div id="projects">
     <div class="row">
-      <!-- Left -->
-      <div class="6u 12u(narrower)">
-        <a id="synthetic-data-generator"></a>
-        <section class="box special">
+      <div id="left" class="6u 12u(narrower)">
+        <div
+          v-for="(item, index) in projects.slice(0, 9)"
+          :key="index"
+          :id="item.id"
+          class="box special"
+        >
           <span class="image featured showcase">
-            <a href="projects/synthetic-data-generator.html">
-              <img src="https://i.imgur.com/oolcato.png" alt>
+            <a :href="`/projects/${item.id}`">
+              <img :src="item.img" alt>
             </a>
           </span>
-          <h3>Synthetic Data Generator</h3>
-          <p>
-            Automating Blender with Python to render photorealistic images/videos of racing tracks to be used as training
-            data and driving simulation of an autonomous vehicle.
-          </p>
+          <h3>{{ item.title }}</h3>
+          <p>{{ item.description }}</p>
           <ul class="actions">
             <li>
-              <a href="projects/synthetic-data-generator.html" class="button alt">Read More</a>
+              <a :href="`/projects/${item.id}`" class="button alt">Read More</a>
             </li>
           </ul>
-        </section>
-
-        <a id="traffic-light-detector"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/traffic-light-detector.html">
-              <img src="https://i.imgur.com/PSrbi1J.jpg" alt>
-            </a>
-          </span>
-          <h3>Traffic Light Detector</h3>
-          <p>A computer vision system to detect traffic light states.</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/traffic-light-detector.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="interframe-compression"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/interframe-compression.html">
-              <img src="https://i.imgur.com/LaqL9Nw.png" alt>
-            </a>
-          </span>
-          <h3>Interframe Compression</h3>
-          <p>
-            A system to send only the difference between the current and previous image in a video feed to reduce
-            bandwidth
-            usage in video streaming.
-          </p>
-          <ul class="actions">
-            <li>
-              <a href="projects/interframe-compression.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="ottabotics-learn"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/ottabotics-learn.html">
-              <img src="https://i.imgur.com/5TTl1fA.jpg" alt>
-            </a>
-          </span>
-          <h3>Ottabotics Learning Project</h3>
-          <p>An introductory team project for uOttawa's robotics club.</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/ottabotics-learn.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="lifes-charge"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/lifes-charge.html">
-              <img src="https://i.imgur.com/appche5.png" alt>
-            </a>
-          </span>
-          <h3>Life’s Charge – A Countdown to Death</h3>
-          <p>
-            A finite existence demonstrated through a visual countdown of a typical 4160-week life. Each dot represents a
-            week, and each row is a year. The video is a perfect loop if repeated; the perpetual cycle is seen as the charge
-            dwindles away to reveal a new life.
-          </p>
-          <ul class="actions">
-            <li>
-              <a href="projects/lifes-charge.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="potato-simulator"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/potato-simulator.html">
-              <img src="https://i.imgur.com/rRFIcrA.png" alt>
-            </a>
-          </span>
-          <h3>Potato Simulator</h3>
-          <p>
-            A silly Unity 3D game that revolves around the player being a high-speed, tumbling potato mindlessly rolling
-            around to explore the world.
-          </p>
-          <ul class="actions">
-            <li>
-              <a href="projects/potato-simulator.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="spells-and-steel"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/spells-and-steel.html">
-              <img src="https://i.imgur.com/bbgMk1Y.png" alt>
-            </a>
-          </span>
-          <h3>Spells and Steel</h3>
-          <p>A silly short film filled with magic and action.</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/spells-and-steel.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="minecraft-art"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/minecraft-art.html">
-              <img src="https://i.imgur.com/sZGC81P.jpg" alt>
-            </a>
-          </span>
-          <h3>Minecraft Art</h3>
-          <p>Digital art that I made for others.</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/minecraft-art.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="pusheen-coins"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/pusheen-coins.html">
-              <img src="https://i.imgur.com/uBfj15Q.png" alt>
-            </a>
-          </span>
-          <h3>Pusheen Coins</h3>
-          <p>Coins based on a cute Internet cat named Pusheen.</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/pusheen-coins.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
+        </div>
       </div>
 
-      <!-- Right -->
-      <div class="6u 12u(narrower)">
-        <a id="piano-tiles-bot"></a>
-        <section class="box special">
+      <div id="right" class="6u 12u(narrower)">
+        <div
+          v-for="(item, index) in projects.slice(9)"
+          :key="index"
+          :id="item.id"
+          class="box special"
+        >
           <span class="image featured showcase">
-            <a href="projects/piano-tiles-bot.html">
-              <img src="https://i.imgur.com/LRvj1Rk.png" alt>
+            <a :href="`/projects/${item.id}`">
+              <img :src="item.img" alt>
             </a>
           </span>
-          <h3>Piano Tiles Bot</h3>
-          <p>Automated gameplay of "Piano Tiles" mobile game with PyAutoGUI.</p>
+          <h3>{{ item.title }}</h3>
+          <p>{{ item.description }}</p>
           <ul class="actions">
             <li>
-              <a href="projects/piano-tiles-bot.html" class="button alt">Read More</a>
+              <a :href="`/projects/${item.id}`" class="button alt">Read More</a>
             </li>
           </ul>
-        </section>
-
-        <a id="habit-visualization"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/habit-visualization.html">
-              <img src="https://i.imgur.com/NgXNej5.png" alt>
-            </a>
-          </span>
-          <h3>Habit Visualization</h3>
-          <p>
-            A python rework of "My terrible sleep habits visualized."
-            It's now more generalized and does not rely on any external programs to create the images.
-          </p>
-          <ul class="actions">
-            <li>
-              <a href="projects/habit-visualization.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="sleep-visualization"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/sleep-visualization.html">
-              <img src="https://i.imgur.com/XRJPNOE.png" alt>
-            </a>
-          </span>
-          <h3>My terrible sleep habits visualized</h3>
-          <p>Sleep visualizations based on manually logged sleep entries.</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/sleep-visualization.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="gotta-go"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/gotta-go.html">
-              <img src="https://i.imgur.com/s7ZxgTP.jpg" alt>
-            </a>
-          </span>
-          <h3>Gotta Go</h3>
-          <p>
-            A mock startup project for SEG2900 where my group was tasked with creating a website and pitch for
-            a fictional company.
-          </p>
-          <ul class="actions">
-            <li>
-              <a href="projects/gotta-go.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="photoshop-battles"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/photoshop-battles.html">
-              <img src="https://i.imgur.com/0fujxDx.jpg" alt>
-            </a>
-          </span>
-          <h3>Photoshop Battles</h3>
-          <p>Friendly Photoshop competitions at /r/photoshopbattles on Reddit.</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/photoshop-battles.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="moon-photos"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/moon-photos.html">
-              <img src="https://i.imgur.com/1MV0JY8.png" alt>
-            </a>
-          </span>
-          <h3>Moon Photo</h3>
-          <p>An attempt to capture the highest quality moon photos possible with an entry-level camera setup.</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/moon-photos.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="photography"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/photography.html">
-              <img src="https://i.imgur.com/qr7ywEy.jpg" alt>
-            </a>
-          </span>
-          <h3>Photography</h3>
-          <p>Pretty pixels?</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/photography.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
-
-        <a id="mtc-training"></a>
-        <section class="box special">
-          <span class="image featured showcase">
-            <a href="projects/mtc-training.html">
-              <img
-                src="https://engineering.uottawa.ca/sites/default/files/brunsfieldlandingpage_0.jpg"
-                alt
-              >
-            </a>
-          </span>
-          <h3>MTC Training</h3>
-          <p>Training done at the uOttawa's Brunsfield Centre.</p>
-          <ul class="actions">
-            <li>
-              <a href="projects/mtc-training.html" class="button alt">Read More</a>
-            </li>
-          </ul>
-        </section>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "projects",
+  data() {
+    return {
+      projects: [
+        {
+          id: `synthetic-data-generator`,
+          img: `https://i.imgur.com/oolcato.png`,
+          title: `Synthetic Data Generator`,
+          description: `Automating Blender with Python to render photorealistic images/videos of racing tracks to be used as training data and driving simulation of an autonomous vehicle.`
+        },
+        {
+          id: `traffic-light-detector`,
+          img: `https://i.imgur.com/PSrbi1J.jpg`,
+          title: `Traffic Light Detector`,
+          description: `A computer vision system to detect traffic light states.`
+        },
+        {
+          id: `interframe-compression`,
+          img: `https://i.imgur.com/LaqL9Nw.png`,
+          title: `Interframe Compression`,
+          description: `A system to send only the difference between the current and previous image in a video feed to reduce bandwidth usage in video streaming.`
+        },
+        {
+          id: `ottabotics-learn`,
+          img: `https://i.imgur.com/5TTl1fA.jpg`,
+          title: `Ottabotics Learning Project`,
+          description: `An introductory team project for uOttawa's robotics club.`
+        },
+        {
+          id: `lifes-charge`,
+          img: `https://i.imgur.com/appche5.png`,
+          title: `Life’s Charge – A Countdown to Death`,
+          description: `A finite existence demonstrated through a visual countdown of a typical 4160-week life. Each dot represents a week, and each row is a year. The video is a perfect loop if repeated; the perpetual cycle is seen as the charge dwindles away to reveal a new life.`
+        },
+        {
+          id: `potato-simulator`,
+          img: `https://i.imgur.com/rRFIcrA.png`,
+          title: `Potato Simulator`,
+          description: `A silly Unity 3D game that revolves around the player being a high-speed, tumbling potato mindlessly rolling around to explore the world.`
+        },
+        {
+          id: `spells-and-steel`,
+          img: `https://i.imgur.com/bbgMk1Y.png`,
+          title: `Spells and Steel`,
+          description: `A silly short film filled with magic and action.`
+        },
+        {
+          id: `minecraft-art`,
+          img: `https://i.imgur.com/sZGC81P.jpg`,
+          title: `Minecraft Art`,
+          description: `Digital art that I made for others.`
+        },
+        {
+          id: `pusheen-coins`,
+          img: `https://i.imgur.com/uBfj15Q.png`,
+          title: `Pusheen Coins`,
+          description: `Coins based on a cute Internet cat named Pusheen.`
+        },
+        {
+          id: `piano-tiles-bot`,
+          img: `https://i.imgur.com/LRvj1Rk.png`,
+          title: `Piano Tiles Bot`,
+          description: `Automated gameplay of "Piano Tiles" mobile game with PyAutoGUI.`
+        },
+        {
+          id: `habit-visualization`,
+          img: `https://i.imgur.com/NgXNej5.png`,
+          title: `Habit Visualization`,
+          description: `A python rework of "My terrible sleep habits visualized." It's now more generalized and does not rely on any external programs to create the images.`
+        },
+        {
+          id: `sleep-visualization`,
+          img: `https://i.imgur.com/XRJPNOE.png`,
+          title: `My terrible sleep habits visualized`,
+          description: `Sleep visualizations based on manually logged sleep entries.`
+        },
+        {
+          id: `gotta-go`,
+          img: `https://i.imgur.com/s7ZxgTP.jpg`,
+          title: `Gotta Go`,
+          description: `A mock startup project for SEG2900 where my group was tasked with creating a website and pitch for a fictional company.`
+        },
+        {
+          id: `photoshop-battles`,
+          img: `https://i.imgur.com/0fujxDx.jpg`,
+          title: `Photoshop Battles`,
+          description: `Friendly Photoshop competitions at /r/photoshopbattles on Reddit.`
+        },
+        {
+          id: `moon-photos`,
+          img: `https://i.imgur.com/1MV0JY8.png`,
+          title: `Moon Photo`,
+          description: `An attempt to capture the highest quality moon photos possible with an entry-level camera setup.`
+        },
+        {
+          id: `photography`,
+          img: `https://i.imgur.com/qr7ywEy.jpg`,
+          title: `Photography`,
+          description: `Pretty pixels?`
+        },
+        {
+          id: `mtc-training`,
+          img: `https://engineering.uottawa.ca/sites/default/files/brunsfieldlandingpage_0.jpg`,
+          title: `MTC Training`,
+          description: `Training done at the uOttawa's Brunsfield Centre.`
+        }
+      ]
+    };
+  }
+};
+</script>
 
 <style scoped>
 @media only screen and (max-width: 736px) {
