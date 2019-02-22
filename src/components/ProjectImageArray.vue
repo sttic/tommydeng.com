39d5b8b7b6bfd7f84f00b9090e5a72193f127b60
sttic <tommyd3689@gmail.com>
2019-02-22 00:24:16 -0500

    added ProjectImageArray component

---
+++ src/components/ProjectImageArray.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box alt">
+    <div class="row no-collapse 50% uniform">
+      <div class="4u" v-for="(image, index) in images" :key="index">
+        <span class="image fit">
+          <a :href="image" target="_blank">
+            <img :src="image">
+          </a>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    images: Array
+  }
+}
+</script>
+
