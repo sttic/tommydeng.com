3f1cff0083fb3e0cc342f2f75dddbf0fec0bebd5
sttic <tommyd3689@gmail.com>
2019-02-22 00:30:59 -0500

    added ProjectText component

---
+++ src/components/ProjectText.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box">
+    <h3>{{ title }}</h3>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    text: String
+  }
+};
+</script>
