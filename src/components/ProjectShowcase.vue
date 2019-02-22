67c2202b2fd100e160cdd0784d8ddba61892d72a
sttic <tommyd3689@gmail.com>
2019-02-21 23:38:15 -0500

    added ProjectShowcase component

---
+++ src/components/ProjectShowcase.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="box">
+    <h3>{{ title || `Showcase` }}</h3>
+    <div class="box alt">
+      <div class="row no-collapse 50% uniform">
+        <div class="4u" v-for="(image, index) in images" :key="index">
+          <span class="image fit">
+            <a :href="image" target="_blank">
+              <img :src="image">
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    images: Array
+  }
+};
+</script>
