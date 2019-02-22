1a7a8fd649fa60100d0285182772ab38724a2d13
sttic <tommyd3689@gmail.com>
2019-02-21 23:07:50 -0500

    added ProjectLinks component

---
+++ src/components/ProjectLinks.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+    <h3>Links</h3>
+    <a v-for="(item, index) in links" :key="index" :href="item.ref" target="_blank">{{ item.name }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    links: Array
+  }
+}
+</script>
