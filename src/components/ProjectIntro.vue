ae2bc7214672ca89bc4ea01ee5c82b7ec13546ba
sttic <tommyd3689@gmail.com>
2019-02-21 23:07:19 -0500

    added ProjectIntro component

---
+++ src/components/ProjectIntro.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="box">
+    <span :class="imageStyle ? imageStyle : `image featured`">
+      <img :src="image">
+    </span>
+    <h3>General</h3>
+    <p v-html="general"></p>
+
+    <div class="row">
+      <div class="6u 12u(mobilep)">
+        <h3>Tools</h3>
+        <ul>
+          <li v-for="(tool, index) in tools" :key="index">
+            <a v-if="tool.link" :href="tool.link">{{ tool.name }}</a>
+            <span v-else>{{ tool.name }}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="6u 12u(mobilep)">
+        <h3>Skills developed</h3>
+        <ul>
+          <li v-for="(skill, index) in skills" :key="index">{{ skill }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    imageStyle: String,
+    general: String,
+    tools: Array,
+    skills: Array
+  }
+}
+</script>
