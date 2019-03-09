b23f9319869acf002034cd03a2fbbe7b0762ffdf
sttic <tommyd3689@gmail.com>
2019-03-09 18:50:11 -0500

    added lazy loading images

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,7 +2,10 @@
   <div class="box special">
     <span class="image featured showcase">
       <router-link :to="link">
-        <img :src="img">
+        <VueClazyLoad :src="img" :ratio="0.0001" :margin="`512px`">
+          <img :src="img">
+          <div class="preloader" slot="placeholder" :style="`padding-bottom: ${aspectRatio}%`"></div>
+        </VueClazyLoad>
       </router-link>
     </span>
     <h3>{{ title }}</h3>
@@ -16,12 +19,23 @@
 </template>
 
 <script>
+import { VueClazyLoad } from "vue-clazy-load";
+
 export default {
+  components: {
+    VueClazyLoad
+  },
   props: {
     link: String,
     img: String,
+    imgSize: Object,
     title: String,
     description: String
+  },
+  computed: {
+    aspectRatio() {
+      return (this.imgSize.y / this.imgSize.x) * 100;
+    }
   }
 };
 </script>
