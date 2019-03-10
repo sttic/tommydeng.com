511f33113e9fe3d2f0e7c2866471cb9173282dac
sttic <tommyd3689@gmail.com>
2019-03-09 22:00:58 -0500

    added loading animation

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,9 +2,16 @@
   <div class="box special">
     <span class="image featured showcase">
       <router-link :to="link">
-        <VueClazyLoad :src="img" :ratio="0.0001" :margin="`512px`" :style="`min-height: ${cardWidth * aspectRatio}px;`">
+        <VueClazyLoad
+          :src="img"
+          :ratio="0.0001"
+          :margin="`512px`"
+          :style="`min-height: ${minHeight}px;`"
+        >
           <img :src="img">
-          <div class="preloader" slot="placeholder" :style="`padding-bottom: ${aspectRatio * 100}%`"></div>
+          <div class="preloader" slot="placeholder">
+            <div class="loading-spinner"></div>
+          </div>
         </VueClazyLoad>
       </router-link>
     </span>
@@ -35,8 +42,39 @@ export default {
   },
   computed: {
     aspectRatio() {
-      return (this.imgSize.y / this.imgSize.x);
+      return this.imgSize.y / this.imgSize.x;
+    },
+    minHeight() {
+      return this.cardWidth * this.aspectRatio;
     }
   }
 };
 </script>
+
+<style scoped>
+.preloader {
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  transform: perspective(1px) translateY(-50%);
+}
+
+.loading-spinner {
+  margin: auto;
+  border: 4px solid #fff;
+  border-top: 4px solid #f5f5f5;
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
