71b86a7c675e7b28e932765d4b94f3a68176aadd
sttic <tommyd3689@gmail.com>
2019-03-28 00:20:48 -0400

    install and add intersection-observer for lazy load browser compatibility

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import { store } from "./store.js";
 
+require('intersection-observer');
+
 Vue.config.productionTip = false;
 
 Vue.mixin({
